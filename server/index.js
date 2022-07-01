411b3e0f88e9cb3c4d7ac23404aa1c8296701b8b
Youssef <youssef_a_fadel@hotmail.com>
2022-07-01 17:15:02 +0200

    added create & read car records

--- server/index.js
+++ server/index.js
@@ -1,12 +1,41 @@
-import express from 'express'
+import express from "express";
+import { PrismaClient, Prisma } from "@prisma/client";
 
-const app =  express()
-const port = 3000
+const prisma = new PrismaClient();
+const app = express();
+const port = 3000;
 
-app.post('/car/reading', (req, res) => {
-  
-})
+app.post("/cars/create", async (req, res) => {
+  const request = req.body;
+  const speed = request.speed;
+  let car;
+
+  if (speed > 60) {
+    car = {
+      image: request.image,
+      plate: request.plate || null,
+      violation: {
+        create: {
+          speed: request.speed,
+        },
+      },
+    };
+  } else {
+    car = {
+      image: request.image,
+      plate: request.plate || null,
+    };
+  }
+
+  const createCar = await prisma.car.create({ data: car });
+  res.send("Record successfully created.");
+});
+
+app.get("/cars/violations", async (req, res) => {
+  const getCars = await prisma.violation.findMany();
+  res.send(getCars);
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
